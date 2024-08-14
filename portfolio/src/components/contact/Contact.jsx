9d1ac4e307702bc26eec2dace784faa37992d3e8
HariPalakollu <palakolluh@gmil.com>
2024-08-14 19:52:11 +0530

    My First Commit

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -42,7 +42,7 @@ export const Contact = () => {
           <article className='contact__option'>
           <FaWhatsapp className='contact__option-icon'/>
             <h4>What's App</h4>
-            <h5>+91 ......0872</h5>
+            <h5>+91 ......30872</h5>
             <a href="https://api.whatsapp.com/send?phone=+919676330872" target='_blank'>Send a Message</a>
           </article>
         </div>
